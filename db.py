58b83bfe9a4e3d43fa17373a655eab3130962464
Andrew <andrewdircks@me.com>
2021-04-20 17:39:50 -0400

    load data with tensorflow sql query

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import tensorflow as tf
 from settings import DEV_DB_PATH
 
 
@@ -6,17 +7,24 @@ class DataModel:
     def __init__(self):
         self.con = sqlite3.connect(DEV_DB_PATH)
 
-    def test(self):
-        cur = self.con.cursor()
-        for i, row in enumerate(
-                cur.execute('SELECT language, snippet FROM snippets;')):
+    def load(self):
+        return tf.data.experimental.SqlDataset(
+            "sqlite",
+            DEV_DB_PATH,
+            "select language, snippet FROM snippets;",
+            (tf.string, tf.string),
+        )
+
+    def test_tf(self):
+        dataset = self.load()
+        for i, elt in enumerate(dataset):
             if i > 100:
                 break
-            print(row)
+            print(elt)
 
     def __exit__(self):
         self.con.close()
 
 
-if __name__ == '__main__':
-    DataModel().test()
+if __name__ == "__main__":
+    DataModel().test_tf()
