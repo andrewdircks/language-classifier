ec7c614ac0ecbbf56990cf31de25c772acf5192c
Andrew <andrewdircks@me.com>
2021-04-21 10:51:33 -0400

    no need for datamodel class

--- db.py
+++ db.py
@@ -3,28 +3,21 @@ import tensorflow as tf
 from settings import DEV_DB_PATH
 
 
-class DataModel:
-    def __init__(self):
-        self.con = sqlite3.connect(DEV_DB_PATH)
-
-    def load(self):
+def load(dev=True):
+    if dev:
         return tf.data.experimental.SqlDataset(
             "sqlite",
             DEV_DB_PATH,
             "select language, snippet FROM snippets;",
             (tf.string, tf.string),
         )
-
-    def test_tf(self):
-        dataset = self.load()
-        for i, elt in enumerate(dataset):
-            if i > 100:
-                break
-            print(elt)
-
-    def __exit__(self):
-        self.con.close()
+    else:
+        raise Exception("Full database not stored")
 
 
 if __name__ == "__main__":
-    DataModel().test_tf()
+    db = load()
+    for i, elt in enumerate(db):
+        if i > 1000:
+            break
+        print(elt)
