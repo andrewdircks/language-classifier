14395d7a03c8cc8c5b7c13b2d554aa373c843a2e
Andrew <andrewdircks@me.com>
2021-04-22 14:27:30 -0400

    data processing by character

--- db.py
+++ db.py
@@ -1,23 +1,63 @@
 import sqlite3
+import numpy as np
 import tensorflow as tf
-from settings import DEV_DB_PATH
+from settings import (
+    DEV_DB_PATH,
+    FULL_DB_PATH,
+    langs_map,
+    chars_map,
+    n_chars,
+    snippet_len,
+)
+
+
+def get_label(entry):
+    """
+    Return this data entry's label as a byte variable.
+    """
+    return tf.Variable(langs_map[entry], dtype="int8")
+
+
+def get_snippet(entry):
+    """
+    Return this data entry's code snippet as a byte vector with.
+    """
+    # read individual characters of tf string
+    entry = tf.strings.unicode_split(entry, "UTF-8").numpy()
+
+    freqs = np.zeros([n_chars], dtype="int8")
+    for i, c in enumerate(entry):
+        if i > snippet_len:
+            break
+        index = chars_map[c.decode("UTF-8").lower()]
+        freqs[index] += 1
+
+    print(freqs)
+    return tf.Variable(freqs, dtype="int8")
 
 
 def load(dev=True):
     if dev:
-        return tf.data.experimental.SqlDataset(
-            "sqlite",
-            DEV_DB_PATH,
-            "select language, snippet FROM snippets;",
-            (tf.string, tf.string),
-        )
+        db_path = DEV_DB_PATH
+    elif FULL_DB_PATH:
+        db_path = FULL_DB_PATH
     else:
         raise Exception("Full database not stored")
 
+    return tf.data.experimental.SqlDataset(
+        "sqlite",
+        db_path,
+        "select language, snippet FROM snippets;",
+        (tf.string, tf.string),
+    )
+
 
 if __name__ == "__main__":
-    db = load()
-    for i, elt in enumerate(db):
+    data = load()
+    data = data.shuffle(1000)
+    for i, elt in enumerate(data):
         if i > 1000:
             break
-        print(elt)
+        v = get_snippet(elt[1])
+        print(v)
+    # get_snippet("")
--- settings.py
+++ settings.py
@@ -1,1 +1,39 @@
 DEV_DB_PATH = "snippets-dev/snippets-dev.db"
+FULL_DB_PATH = None
+
+langs = [
+    "Bash",
+    "C",
+    "C++",
+    "CSV",
+    "DOTFILE",
+    "Go",
+    "HTML",
+    "JSON",
+    "Java",
+    "JavaScript",
+    "Jupyter",
+    "Markdown",
+    "PowerShell",
+    "Python",
+    "Ruby",
+    "Rust",
+    "Shell",
+    "TSV",
+    "Text",
+    "UNKNOWN",
+    "YAML",
+]
+
+chars = "\t\nabcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'/\|_@#$%^&*~`+-=<>()[]{}\" "
+
+snippet_len = 256
+
+
+def _dictify(seq):
+    return {s: i for i, s in enumerate(seq)}
+
+
+langs_map = _dictify(langs)
+chars_map = _dictify(chars)
+n_chars = len(chars)
